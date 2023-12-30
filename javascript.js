14873f4071efedafb3105f1ad595c51c9b0cb0bb
Boxi Z <8356799+Boxis@users.noreply.github.com>
2023-12-30 13:56:44 -0500

    logic for add function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 
 let currentNum = '';
 let firstNum = '';
-let secondNum = '';
+let secondNum = ''; //Do I need a second
 let currentSign = '';
+let tempNum = '';
 
 var input = document.querySelector("#display");
 var buttons = document.querySelectorAll("button");
@@ -10,20 +11,25 @@ var buttons = document.querySelectorAll("button");
 
 // TODO
 /*
-  -keys pressed
-  -keys clicked
+  -keys pressed -> DONE
+  -add -> DONE?
+  -subtract
+  -multiply
+  -divide
+
   -first num is total
   -second num resets display
+  -keys clicked
 */
 
 for (i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function(e) {
     // input.value = input.value + e.currentTarget.value;
     // Figure out how to identify what is clicked
-    console.log(
-      'value', e.target.value,
-      'key', e.target.id
-    );
+    // console.log(
+    //   'value', e.target.value,
+    //   'key', e.target.id
+    // );
     
     if(e.target.value in [1,2,3,4,5,6,7,8,9,0]) {
       input.value = input.value + e.target.value;
@@ -51,55 +57,78 @@ for (i = 0; i < buttons.length; i++) {
 // Tracks numbers pressed with keyboard
 const keyCodes = () => {
   document.addEventListener('keydown', function (e) {
-    console.log(
-      'key', e.key,
-      'code', e.code,
-      'location', e.location
-    );
+    // console.log(
+    //   'key', e.key,
+    //   'code', e.code,
+    //   'location', e.location
+    // );
 
-    // Store numbers pressed to an array
+    if(e.key == '=' || e.key == 'Enter' && currentSign != '') {
+      console.log('equals')
+      if (firstNum != '' && secondNum == ''){
+        secondNum = Number(currentNum);
+        operate(currentSign);
+      } else if (firstNum != '' && secondNum != '') {
+        operate(currentSign);
+      }
+    } else if(e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/') {
+      for (let step = 0; step < 2; step++){
+        if(firstNum != '' && secondNum == '' && currentSign != '') {
+          console.log('setting second number')
+          secondNum = Number(currentNum);
+          operate(currentSign);
+        } else if (firstNum == '') {
+          console.log('setting first number')
+          firstNum = Number(currentNum);
+          currentNum = '';
+          currentSign = e.key;
+        } else if (firstNum != '' && secondNum == '' && currentSign != '') {
+          console.log('op key 2')
+          secondNum = Number(currentNum);
+          console.log('operating')
+          operate(currentSign)
+          firstNum = Number(currentNum)
+        } else if (currentSign != '') {
+          console.log('setting sign');
+          currentSign = e.key;
+        }
+      }
+    } 
+    
     if(e.key in [1,2,3,4,5,6,7,8,9,0]) {
-      // console.log(e.key, e.code);
-      console.log(
-      'key', e.key,
-      'code', e.code,
-      'location', e.location
-    );
+      // console.log('key', e.key, 'code', e.code, 'location', e.location);
+      if (firstNum != '' && currentSign != '') {
+        console.log('clear currentNum & display.value');
+        currentNum = '';
+        display.value = '';
+      }
       currentNum += e.key;
       input.value = input.value + e.key;
     } else if(e.key == '.' && !input.value.includes(".")) {
       currentNum += e.key;
       input.value = input.value + e.key
-    } else if(e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/' ||
-              e.key == '=' || e.key == 'Enter') {
-      console.log(e.key);
-      currentSign = e.key;
-      operate(currentSign);
-      // if (currentSign == '' || currentSign != e.key) {
-      //   currentSign = e.key;
-      //   operate();
-      // } else if (e.key == '=') {
-      //   operate(currentSign);
-      // }
     } else if(e.key == 'Escape') {
       acFunction();
+    } else if(e.key == 'Backspace') {
+      display.value = display.value.slice(0, display.value.length - 1);
+      currentNum = currentNum.slice(0, currentNum.length - 1);
     }
   });
 };
 
-keyCodes();
 
-
-// AC button click
+// All Clear (AC) button click
 document.getElementById("btn-ac").addEventListener("click", acFunction);
 
 function acFunction() {
-  console.log('All Cleared');
+  // console.log('All Cleared');
   currentNum = '';
   firstNum = '';
   secondNum = '';
   currentSign = '';
+  tempNum = '';
   display.value = '';
+  console.clear()
 };
 
 
@@ -134,27 +163,21 @@ function divide(a, b) {
 
 function operate(function_sign) {
 
-  if (firstNum == '') {
-    firstNum = Number(currentNum);
-    currentNum = '';
-    input.value = '';
-    // keyCodes ();
-  } else if (secondNum == '') {
-    secondNum = Number(currentNum);
-    currentNum = '';
-  }
-
   if (firstNum != '' && secondNum != '' && currentSign != '') {
     if (function_sign == '+'){
       firstNum = add(firstNum, secondNum);
       console.log(firstNum);
-      secondNum = '';
       input.value = firstNum;
+      currentNum = input.value;
+      firstNum = '';
+      secondNum = '';
     } else if (function_sign == '-') {
       firstNum = subtract(firstNum, secondNum);
       console.log(firstNum);
-      secondNum = '';
       input.value = firstNum;
+      currentNum = input.value;
+      firstNum = '';
+      secondNum = '';
     } else {
       console.log('hi');
     };
@@ -169,3 +192,4 @@ function operate(function_sign) {
 };
 
 
+keyCodes();
